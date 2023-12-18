04019182027aa64d4c925bc63a75551f8170c924
ZakyAH123 <hakimzaky51@gmail.com>
2023-12-18 13:24:24 +0700

    2

--- js/products.js
+++ js/products.js
@@ -62,23 +62,6 @@ if (categoryContainer) {
   });
 }
 
-/*
-=============
-Product Details Left
-=============
- */
-const pic1 = document.getElementById("pic1");
-const pic2 = document.getElementById("pic2");
-const pic3 = document.getElementById("pic3");
-const pic4 = document.getElementById("pic4");
-const pic5 = document.getElementById("pic5");
-const picContainer = document.querySelector(".product__pictures");
-const zoom = document.getElementById("zoom");
-const pic = document.getElementById("pic");
-
-// Picture List
-const picList = [pic1, pic2, pic3, pic4, pic5];
-
 // Active Picture
 let picActive = 1;
 
@@ -106,35 +89,3 @@ const changeImage = (imgSrc, n) => {
   //   update the active side picture
   picActive = n;
 };
-
-/*
-=============
-Product Details Bottom
-=============
- */
-
-const btns = document.querySelectorAll(".detail-btn");
-const detail = document.querySelector(".product-detail__bottom");
-const contents = document.querySelectorAll(".content");
-
-if (detail) {
-  detail.addEventListener("click", e => {
-    const target = e.target.closest(".detail-btn");
-    if (!target) return;
-
-    const id = target.dataset.id;
-    if (id) {
-      Array.from(btns).forEach(btn => {
-        // remove active from all btn
-        btn.classList.remove("active");
-        e.target.closest(".detail-btn").classList.add("active");
-      });
-      // hide other active
-      Array.from(contents).forEach(content => {
-        content.classList.remove("active");
-      });
-      const element = document.getElementById(id);
-      element.classList.add("active");
-    }
-  });
-}
